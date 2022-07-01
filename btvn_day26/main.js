45b0ca998adb86df2aafaf3354124f3a9a776eb4
VuVietCuong10062006 <104380320+VuVietCuong10062006@users.noreply.github.com>
2022-07-01 21:42:25 +0700

    update btvn day 26

--- btvn_day26/main.js
+++ btvn_day26/main.js
@@ -8,6 +8,11 @@ $("#overlay").click(function(){
     $("#overlay").css("display","none")
 })
 
+$(".menu-close").click(function(){
+    $(".menu ul").css("left","-250px")
+    $("#overlay").css("display","none")
+})
+
 window.addEventListener('resize',() =>{
     if(window.innerWidth > 768){
         $(".menu ul").css("left","-250px")
