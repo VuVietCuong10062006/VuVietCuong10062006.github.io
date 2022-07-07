0482f7fe70237fc45148711e9d91c0c55de58ed1
VuVietCuong10062006 <104380320+VuVietCuong10062006@users.noreply.github.com>
2022-07-07 22:28:49 +0700

    kiemtra_day30

---
+++ kiemtra_day30/main.js
@@ -0,0 +1,21 @@
+$(".header-menu-icon").click(function(){
+    $(".header .menu ul").css("left","0")
+    $("#overlay").css("display","block")
+})
+
+$("#overlay").click(function(){
+    $(".menu ul").css("left","-250px")
+    $("#overlay").css("display","none")
+})
+
+$(".menu-close").click(function(){
+    $(".menu ul").css("left","-250px")
+    $("#overlay").css("display","none")
+})
+
+// window.addEventListener('resize',() =>{
+//     if(window.innerWidth > 768){
+//         $(".menu ul").css("left","-250px")
+//         // $("#overlay").css("display","none")
+//     }
+// })
