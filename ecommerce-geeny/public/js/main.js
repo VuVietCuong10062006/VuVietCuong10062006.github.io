40be2c8a1c2f545cf739d773d179ff9d06c09df5
VuVietCuong10062006 <104380320+VuVietCuong10062006@users.noreply.github.com>
2022-07-30 15:15:14 +0700

    trang chủ

---
+++ ecommerce-geeny/public/js/main.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+    $('.banner').slick({
+        autoplay:true,
+        arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
+});
+
+$('.testimonial-slick').slick({
+    centerMode: true,
+    centerPadding: '250px',
+    slidesToShow: 1,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa          fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
